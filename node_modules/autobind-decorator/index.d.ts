7b866791b2ec40eead8c92b0584c2f4f3652b17a
alexandrebakercode <alex@bakerwong.com>
2023-02-21 07:19:28 +0000

    cleanup

--- node_modules/autobind-decorator/index.d.ts
+++
@@ -1,6 +0,0 @@
-declare const autobind: ClassDecorator & MethodDecorator;
-
-export declare const boundMethod: MethodDecorator;
-export declare const boundClass: ClassDecorator;
-
-export default autobind;
